7d68c1d1de92e9f228184fdaa10ac248a212e149
Jesse Rodarte <jerodarte@gmail.com>
2019-04-07 19:44:38 -0700

    Update ty.php

--- CNIT133/hw5/ty.php
+++ CNIT133/hw5/ty.php
@@ -1,9 +1,3 @@
 <?php
-  // The global $_POST variable allows you to access the data sent with the POST method by name
-  // To access the data sent with the GET method, you can use $_GET
-  $text = htmlspecialchars($_POST['text']);
-  $radio = htmlspecialchars($_POST['radio']);
-  $check  = htmlspecialchars($_POST['check']);
-  $option = htmlspecialchars($_POST['option']);
-  echo 'text: ' $text, ' ', $radio, ' ', $check, ' ', $option;
+  echo "Thank you for submitting your form!!!";
 ?>
